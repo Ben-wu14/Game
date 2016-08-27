17a0a6491383877019dad310830de32fff58238c
Ben <845687624@qq.com>
2016-08-27 20:20:11 +0800

    Set Id for textView and create choosing function

--- app/src/main/java/com/example/ben/game/Game2.java
+++ app/src/main/java/com/example/ben/game/Game2.java
@@ -1,14 +1,17 @@
 package com.example.ben.game;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.ColorRes;
 import android.util.DisplayMetrics;
+import android.view.View;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 public class Game2 extends Activity {
-
+    int last_id=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +28,28 @@ public class Game2 extends Activity {
         {
             TableRow tableRow=new TableRow(this);
             for (j=0;j<9;j++){
-                TextView textView=new TextView(this);
+                final TextView textView=new TextView(this);
                 textView.setText(""+a[i][j]);
+                textView.setId(i*10+j);
                 textView.setWidth(width);
                 textView.setHeight(width);
                 textView.setGravity(0x11);
+                textView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        textView.setBackgroundColor(Color.parseColor("#B3E5FC"));
+                        int co=textView.getDrawingCacheBackgroundColor();
+                        TextView pre=(TextView)findViewById(last_id);
+                        pre.setBackgroundColor(co);
+                        last_id=textView.getId();
+                    }
+                });
+                textView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                    @Override
+                    public void onFocusChange(View v, boolean hasFocus) {
+                        textView.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                    }
+                });
                 tableRow.addView(textView);
             }
             layout.addView(tableRow);
