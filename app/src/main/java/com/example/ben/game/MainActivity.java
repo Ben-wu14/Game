88d592813e9164575a034ccb14b96fd14467d744
Ben <845687624@qq.com>
2016-08-28 17:53:33 +0800

    Make the Jump between pages and Create simple layout

--- app/src/main/java/com/example/ben/game/MainActivity.java
+++ app/src/main/java/com/example/ben/game/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ben.game;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +28,29 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Button start=(Button)findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ;
+            }
+        });
+        Button difficult=(Button)findViewById(R.id.difficulty);
+        difficult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(MainActivity.this,Difficulty.class);
+                startActivity(i);
+            }
+        });
+        Button help=(Button)findViewById(R.id.help);
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(MainActivity.this,Help.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
@@ -34,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
