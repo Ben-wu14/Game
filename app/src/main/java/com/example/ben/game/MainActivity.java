060a2cea945468941c1cc796a464f42ce7e912b1
ben wu <845687624@qq.com>
2016-09-07 19:21:43 +0800

    Failed to Add the button method in MainActivity(Fixed)

--- app/src/main/java/com/example/ben/game/MainActivity.java
+++ app/src/main/java/com/example/ben/game/MainActivity.java
@@ -61,6 +61,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    public void button(View view){
+        Intent i=new Intent(this,MainpageActivity.class);
+        startActivity(i);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
