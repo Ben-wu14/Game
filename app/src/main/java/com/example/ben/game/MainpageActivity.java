31a1a0e3d28435e697ad4817b876f4b95cbd2b65
ben wu <ben wu>
2016-09-07 17:58:16 +0800

    Create a new public EditText Cancle the Declearation of EditText which is Created-- --before the edit text

--- app/src/main/java/com/example/ben/game/MainpageActivity.java
+++ app/src/main/java/com/example/ben/game/MainpageActivity.java
@@ -7,24 +7,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 public class MainpageActivity extends Activity {
-    EditText ed=(EditText)findViewById(R.id.edit);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
+        ed=(EditText)findViewById(R.id.edit);
         Button button_log=(Button)findViewById(R.id.button_log);
         button_log.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences sp=getSharedPreferences("Login",MODE_PRIVATE);
-                SharedPreferences.Editor editor =sp.edit();
-                editor.putString("name",ed.getText().toString());
+                SharedPreferences sp = getSharedPreferences("Login", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("name", ed.getText().toString());
                 editor.commit();
-                Intent i=new Intent(MainpageActivity.this,MainActivity.class);
+                Intent i = new Intent(MainpageActivity.this, MainActivity.class);
                 startActivity(i);
             }
         });
     }
+    private EditText ed;
 }
