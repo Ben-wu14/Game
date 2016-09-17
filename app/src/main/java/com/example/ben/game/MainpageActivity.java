638b2b2371fff32cc173b03521a2b0e6051c2733
ben wu <845687624@qq.com>
2016-09-17 23:19:40 +0800

    Add comparison of password and username in login page

--- app/src/main/java/com/example/ben/game/MainpageActivity.java
+++ app/src/main/java/com/example/ben/game/MainpageActivity.java
@@ -8,6 +8,7 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -17,23 +18,27 @@ public class MainpageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
-        ArrayList<User>user_data=new ArrayList<>();
+        final ArrayList<User>user_data=new ArrayList<>();
         user_data.add(new User("Ben", "ben1417"));
         user_data.add(new User("Jerry", "Jerry"));
         user_data.add(new User("Ten","Ten"));
-        ed=(EditText)findViewById(R.id.edit);
+        nam=(EditText)findViewById(R.id.edit);
+        pas=(EditText)findViewById(R.id.password);
         Button button_log=(Button)findViewById(R.id.button_log);
         button_log.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences sp = getSharedPreferences("Login", MODE_PRIVATE);
+                /*SharedPreferences sp = getSharedPreferences("Login", MODE_PRIVATE);
                 SharedPreferences.Editor editor = sp.edit();
-                editor.putString("name", ed.getText().toString());
-                editor.commit();
+                editor.putString("name", nam.getText().toString());
+                editor.commit();*/
+                if(user_data.contains(new User(nam.getText().toString(),pas.getText().toString()))){
                 Intent i = new Intent(MainpageActivity.this, MainActivity.class);
-                startActivity(i);
+                startActivity(i);}
+                else Toast.makeText(MainpageActivity.this,"Your account is not exist or Incorrect passwords",Toast.LENGTH_SHORT).show();
             }
         });
     }
-    private EditText ed;
+    private EditText nam;
+    private EditText pas;
 }
