d15e7a2dc048c50a1fc384e2006d4a46883cebb4
Ben <845687624@qq.com>
2016-09-23 21:47:25 +0800

    Save the number of difficulty in the device

--- app/src/main/java/com/example/ben/game/Difficulty.java
+++ app/src/main/java/com/example/ben/game/Difficulty.java
@@ -2,22 +2,31 @@ package com.example.ben.game;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class Difficulty extends Activity {
+    SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_difficulty);
+
+        SharedPreferences sp = getSharedPreferences("Login", MODE_PRIVATE);
+        editor = sp.edit();
+
         Button easy=(Button)findViewById(R.id.easy);
         easy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i=new Intent(Difficulty.this,Game2.class);
-                startActivity(i);
                 difficulty=1;
+                editor.putInt("difficulty",difficulty);
+                editor.commit();
+                startActivity(i);
+
             }
         });
         Button middle=(Button)findViewById(R.id.middle);
@@ -25,8 +34,11 @@ public class Difficulty extends Activity {
             @Override
             public void onClick(View v) {
                 Intent i=new Intent(Difficulty.this,Game2.class);
-                startActivity(i);
                 difficulty=2;
+                editor.putInt("difficulty",difficulty);
+                editor.commit();
+                startActivity(i);
+
             }
         });
         Button diff=(Button)findViewById(R.id.difficult);
@@ -34,8 +46,11 @@ public class Difficulty extends Activity {
             @Override
             public void onClick(View v) {
                 Intent i=new Intent(Difficulty.this,Game2.class);
-                startActivity(i);
                 difficulty=3;
+                editor.putInt("difficulty",difficulty);
+                editor.commit();
+                startActivity(i);
+
             }
         });
     }
