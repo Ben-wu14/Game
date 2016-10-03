89c3e140b494d00315801c097f4a9f012e4147c3
Ben <845687624@qq.com>
2016-10-03 16:19:18 +0800

    Add new class with all infor for a game

---
+++ app/src/main/java/com/example/ben/game/ArrayFile.java
@@ -0,0 +1,39 @@
+package com.example.ben.game;
+
+/**
+ * Created by ben on 2016/10/3.
+ */
+public class ArrayFile {
+    private ArrayData data;
+    private int[][] userAnser;
+    private int difficulty;
+    private int total_blank;
+    private int min;
+    private int sec;
+    public ArrayFile(ArrayData data,int[][]userAnser,int difficulty,int total_blank,int min,int sec){
+        this.data=data;
+        this.userAnser=userAnser;
+        this.difficulty=difficulty;
+        this.total_blank=total_blank;
+        this.min=min;
+        this.sec=sec;
+    }
+    public ArrayData getData(){
+        return data;
+    }
+    public int[][] getUserAnser(){
+        return userAnser;
+    }
+    public int getDifficulty(){
+        return difficulty;
+    }
+    public int getTotal_blank(){
+        return total_blank;
+    }
+    public int getMin(){
+        return min;
+    }
+    public int getSec(){
+        return sec;
+    }
+}
