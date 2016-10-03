d1a3aa34fe955c65dbcf48dfa79bfc4843e6a326
Ben <845687624@qq.com>
2016-10-03 22:20:55 +0800

    Let the Start button jump to a listView

--- app/src/main/java/com/example/ben/game/MainActivity.java
+++ app/src/main/java/com/example/ben/game/MainActivity.java
@@ -35,8 +35,8 @@ public class MainActivity extends AppCompatActivity {
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, Game2.class);
-                startActivityForResult(i,1);
+                Intent i = new Intent(MainActivity.this, GameList.class);
+                startActivity(i);
             }
         });
         Button difficult=(Button)findViewById(R.id.difficulty);
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, Difficulty.class);
-                startActivity(i);
+                startActivityForResult(i,1);
             }
         });
         Button help=(Button)findViewById(R.id.help);
