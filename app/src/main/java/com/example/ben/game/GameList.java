fa99ce21194d0b3e555623629d255cc75e512b4a
Ben <845687624@qq.com>
2016-10-03 16:20:17 +0800

    Adding get infor from the doc

--- app/src/main/java/com/example/ben/game/GameList.java
+++ app/src/main/java/com/example/ben/game/GameList.java
@@ -4,14 +4,62 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Activity;
 
-public class GameList extends Activity {
-String username;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
+public class GameList extends Activity {
+    String username;
+    ArrayData data;
+    int[][] userAnser;
+    int difficulty;
+    int total_blank;
+    int min;
+    int sec;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_list);
         getUsername();
+
+        SharedPreferences sp=getSharedPreferences("filePath",MODE_APPEND);
+        String path=sp.getString("path","");
+
+        ArrayList<ArrayFile> arrayFileArrayList=new ArrayList<>();
+        String filePath = path + username+"game.dat";
+        File f=new File(filePath);
+        if (!f.exists())
+        {
+            try
+            {
+                f.createNewFile();
+            }
+            catch (IOException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        try{
+            ObjectInputStream input=new ObjectInputStream(new BufferedInputStream(new FileInputStream(filePath)));
+            Object object;
+            while((object=input.readObject()) != null){
+                data=(ArrayData)object;
+                userAnser=(int[][])input.readObject();
+                difficulty=input.readInt();
+                total_blank=input.readInt();
+                min=input.readInt();
+                sec=input.readInt();
+                arrayFileArrayList.add(new ArrayFile(data,userAnser,difficulty,total_blank,min,sec));
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
     }
     public void getUsername(){
         SharedPreferences sp=getSharedPreferences("Login", MODE_PRIVATE);
