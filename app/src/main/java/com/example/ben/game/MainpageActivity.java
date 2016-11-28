55d9c4f93d206b199685bb98642462da4250d44a
ben wu <845687624@qq.com>
2016-11-28 21:50:30 +0800

    Fix the back key in login

--- app/src/main/java/com/example/ben/game/MainpageActivity.java
+++ app/src/main/java/com/example/ben/game/MainpageActivity.java
@@ -56,4 +56,11 @@ public class MainpageActivity extends Activity {
     }
     private EditText nam;
     private EditText pas;
+    @Override
+    public void onBackPressed() {
+        Intent setIntent = new Intent(Intent.ACTION_MAIN);
+        setIntent.addCategory(Intent.CATEGORY_HOME);
+        setIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(setIntent);
+    }
 }
